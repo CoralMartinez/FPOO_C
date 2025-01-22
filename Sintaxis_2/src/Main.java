09a7d379e1bedb7cddf0d0941fc823203f078fde
CoralMartinez <coralmtzs084@gmail.com>
2025-01-22 14:42:32 -0600

    Estructura de un archivo Java y manejo de cadenas

--- Sintaxis_2/src/Main.java
+++ Sintaxis_2/src/Main.java
@@ -1,11 +1,42 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//Clase principal
 public class Main {
+
+    /*comentario
+
+    comentario
+
+    comentario*/
+
+
+    //Metodo main
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+
+        /*Comentario
+        de varias
+        líneas
+         */
+
         System.out.println("Hello World!");
         System.out.println("Hola, Coral Martínez Silvestre");
+
+
+        String cadenas = "Coral " + "Martinez " + "Silvestre";
+
+        System.out.println(cadenas);
+
+        System.out.println(cadenas.length());
+
+        System.out.println(cadenas.substring(2,5));
+
+        System.out.println(cadenas.substring(2));//solo se coloca el 2 y el beginindex aparece de forma automatica
+
+
+        System.out.println(cadenas.substring(0,5));
 
 
     }
