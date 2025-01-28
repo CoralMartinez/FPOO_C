54e50ef62b0b4309941cec1fdd6b01ef7b2ce10c
CoralMartinez <coralmtzs084@gmail.com>
2025-01-28 16:50:24 -0600

    Se crea el proyecto "proyecto4_juegos_edad"

--- practica4_numero_par_impar/src/Main.java
+++ practica4_numero_par_impar/src/Main.java
@@ -16,9 +16,15 @@ public class Main {
         System.out.println("Ingresa un número: ");
         int numero = sca.nextInt();
 
-        System.out.println(numero);
+        if(numero % 2 == 0){
+            System.out.println("El número es par");
+        }
 
+        else{
+            System.out.println("El número es impar");
+        }
 
+        //Fin programa 2
 
     }
 }
