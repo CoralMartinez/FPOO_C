a107f604ab59e8a4bbcfbedcec7916062ad6396d
CoralMartinez <coralmtzs084@gmail.com>
2025-01-28 16:41:49 -0600

    Se crea el proyecto "practica4_numero_par_impar"

--- practica 4/src/Main.java
+++ practica 4/src/Main.java
@@ -1,15 +1,38 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+       Scanner sca = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+       //Inicio programa 1
+
+        System.out.println("Programa 1. Contraseña");
+
+        String contra = "Hola"+"usuario"+ ":)";
+
+
+        System.out.println("Ingresa la contraseña: ");
+
+        String contra1 =sca.next();
+
+        if(contra.equalsIgnoreCase(contra1)){
+            System.out.println("La contraseña es correcta");
         }
-    }
-}
+
+        else{
+            System.out.println("La contraseña no es correcta");
+        }
+
+
+        System.out.println("Contraseña: "+contra);
+
+        //Fin programa 1
+
+    }//Llave fin public static
+}//Llave fin Main
